c3649a67c4cdd31522839fb431f4bd95efdbc9c3
Zuhair <84449586+MohammedZuhairAhmed@users.noreply.github.com>
2023-12-31 12:08:32 +0530

    Update file_reader.py

--- file_reader.py
+++ file_reader.py
@@ -1,4 +1,7 @@
-file_path = input("Enter the path of the file: ")
+file_path = input("Enter the name of the file: ")
+
+# Prefix the file path with /home/ec2-user
+file_path = "/home/ec2-user" + file_path
 
 try:
     with open(file_path, 'r') as file:
